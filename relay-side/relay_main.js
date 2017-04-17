6957104bd47e46930516f37fe1d06588cfa95a38
giuliojiang <giuliojiang@gmail.com>
2017-04-17 17:23:54 +0100

    writing main file of relay

---
+++ relay-side/relay_main.js
@@ -0,0 +1,49 @@
+var rawconf = require("rawconf");
+var async = require("async");
+
+async.waterfall([
+
+    // Setup configuration file
+    function(callback) {
+        rawconf.set_configuration_file(1, "/config/config.json", (err) => {
+            if (err) {
+                callback(err);
+                return;
+            }
+            
+            console.log("Configuration file successfully loaded");
+            
+            async.setImmediate(() => {
+                callback(null);
+            });
+            return;
+        });
+    },
+    
+    // Start the external network listener
+    function(callback) {
+        console.log("External network listener not implemented yet");
+        
+        async.setImmediate(() => {
+            callback(null);
+        });
+        return;
+    },
+    
+    // Start the tunnel network listener
+    function(callback) {
+        console.log("Tunnel network listener not implemented yet");
+        
+        async.setImmediate(() => {
+            callback(null);
+        });
+        return;
+    }
+
+], (err, res) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("All components started");
+    }
+});
