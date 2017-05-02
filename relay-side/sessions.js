73d365e8ba99bda9d1aac16862c24e3a3b8eb967
giuliojiang <giuliojiang@gmail.com>
2017-05-02 20:34:23 +0100

    Add maximum clients limit

--- relay-side/sessions.js
+++ relay-side/sessions.js
@@ -19,14 +19,18 @@ When a client sends data:
 
 
 var BiMap = require("bimap");
+var rawconf = require("rawconf");
 var relay_listener = require(__dirname + "/relay_listener.js");
 var data_encoder = require(__dirname + "/data_encoder.js");
+
+var config = rawconf.get_config();
 
 // //////////
 // Data
 // //////////
 
 var clients = new BiMap;
+var clients_size = 0;
 
 var next_client_number = 0;
 
@@ -37,6 +41,12 @@ var next_client_number = 0;
 // Handle a new client that connected
 // Generate new sequence number and add to the bidirectional map
 module.exports.client_connected = function(socket) {
+    if (clients_size >= config.relay_max_clients) {
+        console.log("Too many clients connected, rejecting connection");
+        socket.destroy();
+        return;
+    }
+    
     socket.name = socket.remoteAddress + ":" + socket.remotePort;
     
     var current_client_number = next_client_number;
@@ -44,7 +54,7 @@ module.exports.client_connected = function(socket) {
     
     console.log("Connected client ["+socket.name+"]");
     
-    clients.push(socket.name, [socket, current_client_number]);
+    add_to_map(socket.name, [socket, current_client_number]);
 };
 
 // Receive data from a client
@@ -76,7 +86,7 @@ module.exports.client_disconnected = function(socket) {
     var socketname = socket.name;
     var clientnumber = clients.key(socketname)[1];
 
-    clients.removeKey(socket.name);
+    remove_from_map(socket.name);
     
     // Send special message for client disconnect
     console.log("Sending disconnect message for client ["+clientnumber+"]");
@@ -94,9 +104,19 @@ module.exports.close_client_connection = function(cid) {
     }
     var csocket = csocket_val[0];
     csocket.destroy();
-    clients.removeKey(cname);
+    remove_from_map(cname);
 };
 
 // //////////
 // Private methods
 // //////////
+
+var add_to_map = function(k, v) {
+    clients.push(k, v);
+    clients_size += 1;
+};
+
+var remove_from_map = function(k) {
+    clients.removeKey(k);
+    clients_size -= 1;
+};
